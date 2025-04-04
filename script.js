393b9c57ef6a4ad8ee2618096f2a1cd48b3f268b
Luca Cecchini <126694762+cecchiniluca@users.noreply.github.com>
2025-04-04 11:45:51 -0300

    add index.html and script.js with code to play a single round

---
+++ script.js
@@ -0,0 +1,35 @@
+const humanScore = 0;
+const computerScore = 0;
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "rock"
+    } else if (computerChoice === 1) {
+        return "scissors"
+    } else {
+        return "paper"
+    }
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Enter rock, scissors, or paper").trim().toLowerCase();
+}
+
+getHumanChoice()
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win this round. ${humanChoice} beats ${computerChoice}!`);
+    } else {
+        console.log("You lose this round!")
+    }
+}
+
+playRound()

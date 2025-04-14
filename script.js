4fe2f1a1105ff7e18d8756bffeaa71be2d9a75af
Luca Cecchini <126694762+cecchiniluca@users.noreply.github.com>
2025-04-14 12:18:20 -0300

    fix fixPlayRound and add console prints

--- script.js
+++ script.js
@@ -1,39 +1,82 @@
-const humanScore = 0;
-const computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+let winners = [];
 
-function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        return "rock"
-    } else if (computerChoice === 1) {
-        return "scissors"
-    } else {
-        return "paper"
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+  }
+  document.querySelector("button").textContent = "Restart Game";
+  logWins();
+}
+
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+}
+
+function playerChoice() {
+  let input = prompt("Type rock, paper or scissors.");
+  // reload prompt in case user press cancel
+  while (input == null) {
+    input = prompt("Type rock, paper or scissors.");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  // reload prompt in case of misspelling
+  while (check == false) {
+    input = prompt(
+      "Type rock, paper or scissors. Spelling must be exact, but capitalization doesnt matter."
+    );
+    while (input == null) {
+      input = prompt("Type rock, paper or scissors.");
     }
+    input = input.toLowerCase();
+    // restart the function, otherwise check will never be set to true
+    check = validateInput(input);
+  }
+  return input;
 }
 
-function getHumanChoice() {
-    const humanChoice = prompt("Enter rock, scissors, or paper").trim().toLowerCase();
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-getHumanChoice();
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!")
-    } else if (
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        console.log(`You win this round. ${humanChoice} beats ${computerChoice}!`);
-        humanScore++
-    } else {
-        console.log("You lose this round!")
-        computerScore++;
-    }
+function validateInput(choice) {
+  return choices.includes(choice);
 }
 
-function playGame() {
-    
+function checkWinner(choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    return "Tie";
+  } else if (
+    (choiceP === "rock" && choiceC === "scissors") ||
+    (choiceP === "paper" && choiceC === "rock") ||
+    (choiceP === "scissors" && choiceC === "paper")
+  ) {
+    return "Player";
+  } else {
+    return "Computer";
+  }
+}
+
+function logWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+  console.log("Results:");
+  console.log("Player Wins:", playerWins);
+  console.log("Computer Wins:", computerWins);
+  console.log("Ties", ties);
+}
+
+// display who won each round
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round:", round);
+  console.log("Player Chose:", playerChoice);
+  console.log("Compuetr Chose:", computerChoice);
+  console.log(winner, "won the round!");
+  console.log("----------------------------");
 }
